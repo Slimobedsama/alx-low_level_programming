5ff7b449b0fb22a215e002e5c4dc5f12868ad757
slimobedsama <ohwo.okoro@yahoo.com>
2022-09-22 12:26:09 -0700

    print alphabet with omission of q and e added

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Description: Prints alphabet in lowercase except for q and e
+*Return: 0
+*/
+
+int main(void)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
