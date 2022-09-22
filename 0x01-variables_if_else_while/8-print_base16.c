e38c3ca8551353fb42c80a8bc3680b0c97294c55
slimobedsama <ohwo.okoro@yahoo.com>
2022-09-22 13:33:30 -0700

    Print base file added

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+*main -Entry point
+*
+*Description: Prints all the numbers of base 16 in lowecase
+*Return: 0
+*/
+int main(void)
+{
+	char c;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+	for (c = 'a'; c < 'g'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
