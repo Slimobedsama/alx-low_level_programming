f6d1790df0283359161cb61e9666765394dc4d55
slimobedsama <ohwo.okoro@yahoo.com>
2022-09-22 04:58:17 -0700

    printf file added

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: It prints out "with proper grammar, the outcome is a piece of art"
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, the outcome is a piece of art,\n");
+	return(0);
+}
