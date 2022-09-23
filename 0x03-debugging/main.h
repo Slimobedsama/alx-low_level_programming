64bda606fe3ee7fcd17d668aad13e1862ae533ea
slimobedsama <ohwo.okoro@yahoo.com>
2022-09-23 12:31:43 -0700

    main c and main h file added

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,9 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
-void print_remaing_days(int month, int day, int year);
-int convert_day(int month, int day);
+#include <stdio.h>
 
-#endif
+void positive_or_negative(int i);
+
+
+#endif/*MAIN_H*/
