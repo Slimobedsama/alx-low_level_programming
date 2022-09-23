79e9063b9d072fd050bfa33638f6966bbbac5b59
slimobedsama <ohwo.okoro@yahoo.com>
2022-09-23 11:06:17 -0700

    print_most_numbers file added

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
 * print_numbers - print the numbers from 0 to 9
@@ -8,8 +8,10 @@ void print_numbers(void)
 {
 	int i;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i < 10; i++)
+	{
 		_putchar(i + '0');
-
+		i++;
+	}
 	_putchar('\n');
 }
